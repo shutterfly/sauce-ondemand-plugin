dfac97e8055d2ef1127fbe55645a25f14a16f744
Stephen Kuenzli <skuenzli@shutterfly.com>
2014-07-10 10:33:38 -0700

    Add a test to assist with inspecting the environment.

---
+++ src/test/java/hudson/plugins/sauce_ondemand/EnvironmentTest.java
@@ -0,0 +1,26 @@
+package hudson.plugins.sauce_ondemand;
+
+import org.junit.Test;
+
+import java.util.Map;
+
+public class EnvironmentTest {
+
+    @Test
+    public void list_environment_vars() {
+        System.out.println("Listing Environment Variables");
+        for (Map.Entry<String, String> entry : System.getenv().entrySet()) {
+            System.out.println(String.format("%s:%s", entry.getKey(), entry.getValue()));
+        }
+    }
+
+    @Test
+    public void list_system_properties() {
+        System.out.println("Listing System Properties");
+
+        for (Map.Entry<Object, Object> entry : System.getProperties().entrySet()) {
+            System.out.println(String.format("%s:%s", entry.getKey(), entry.getValue()));
+        }
+
+    }
+}
