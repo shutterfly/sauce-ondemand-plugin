56ec8f2e3135b60d0424f49f4977542bf93f110a
Stephen Kuenzli <skuenzli@shutterfly.com>
2014-07-15 09:11:06 -0700

    Refactor TunnelHolder so that is serializable again.  Introduced a (Serializable) TunnelManagerFactory that can be used to create the appropriate AbstractSauceTunnelManager on the slave node, when needed.

--- src/test/java/hudson/plugins/sauce_ondemand/TunnelHolderTest.java
+++ src/test/java/hudson/plugins/sauce_ondemand/TunnelHolderTest.java
@@ -4,38 +4,38 @@ import com.saucelabs.ci.sauceconnect.AbstractSauceTunnelManager;
 import hudson.model.TaskListener;
 import org.apache.commons.lang.RandomStringUtils;
 import org.junit.Test;
-import org.mockito.Mockito;
 
 import java.io.PrintStream;
 
-import static org.junit.Assert.*;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.*;
 
 public class TunnelHolderTest {
 
-
     @Test
     public void constructor_should_store_parameters(){
-        AbstractSauceTunnelManager tunnelManager = Mockito.mock(AbstractSauceTunnelManager.class);
+        SauceOnDemandBuildWrapper.TunnelManagerFactory tunnelManagerFactory = new SauceOnDemandBuildWrapper.TunnelManagerFactory(false);
         String username = anyUsername();
         String options = anyOptionsWithTunnelIdentifier();
-        SauceOnDemandBuildWrapper.TunnelHolder holder = new SauceOnDemandBuildWrapper.TunnelHolder(tunnelManager, username, options);
+        SauceOnDemandBuildWrapper.TunnelHolder holder = new SauceOnDemandBuildWrapper.TunnelHolder(tunnelManagerFactory, username, options);
 
-        assertEquals(tunnelManager, holder.tunnelManager);
+        assertEquals(tunnelManagerFactory, holder.tunnelManagerFactory);
         assertEquals(username, holder.username);
         assertEquals(options, holder.options);
     }
 
     @Test
-    public void close_should_invoke_closeTunnelsForPlan_on_tunnelManager(){
-        AbstractSauceTunnelManager tunnelManager = Mockito.mock(AbstractSauceTunnelManager.class);
+    public void close_should_invoke_closeTunnelsForPlan_on_tunnelManager() throws Exception {
+        AbstractSauceTunnelManager tunnelManager = mock(AbstractSauceTunnelManager.class);
+        SauceOnDemandBuildWrapper.TunnelManagerFactory tunnelManagerFactory = mock(SauceOnDemandBuildWrapper.TunnelManagerFactory.class);
+        when(tunnelManagerFactory.getSauceTunnelManager()).thenReturn(tunnelManager);
+
         String username = anyUsername();
         String options = anyOptionsWithTunnelIdentifier();
 
-        SauceOnDemandBuildWrapper.TunnelHolder holder = new SauceOnDemandBuildWrapper.TunnelHolder(tunnelManager, username, options);
+        SauceOnDemandBuildWrapper.TunnelHolder holder = new SauceOnDemandBuildWrapper.TunnelHolder(tunnelManagerFactory, username, options);
 
-        TaskListener taskListener = Mockito.mock(TaskListener.class);
+        TaskListener taskListener = mock(TaskListener.class);
 
         PrintStream printStream = new PrintStream(System.out);
         when(taskListener.getLogger()).thenReturn(printStream);
